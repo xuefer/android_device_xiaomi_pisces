7404bfb45267da8a37286a034e39b6d1a538f6c6
Xuefer <xuefer@gmail.com>
2015-04-11 01:06:23 +0800

    patch tool: add color, hide detail if already patch

--- patch/applypatch.sh
+++ patch/applypatch.sh
@@ -7,9 +7,14 @@
 
 localdir=$(cd "$(dirname "$0")" && pwd)
 topdir=$(cd "$localdir/../../../.." && pwd)
-
-(cd $localdir; find . -iname '*.patch' -type f) | while read patch; do
-	echo " *** Applying patch $patch"
-	patch "$@" -p1 -N -i "$localdir/$patch" -r - -d "$topdir/$(dirname "$patch")/"
+(cd $localdir; find . -iname '*.patch' -type f) | while read relpatch; do
+	dir="$topdir/$(dirname "$relpatch")/"
+	patch="$localdir/$relpatch"
+	if patch --dry-run -p1 -N -i "$patch" -r - -d "$dir" | grep -v 'Skipping patch\|hunks\? ignored\|patching file ' | grep . > /dev/null; then
+		echo "[34m*** Applying patch $relpatch[0m"
+		patch "$@" -p1 -N -i "$patch" -r - -d "$dir"
+	else
+		echo "[32m*** skipping already applied $relpatch[0m"
+	fi
 done
 
