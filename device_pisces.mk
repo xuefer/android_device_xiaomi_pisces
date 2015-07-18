325b2e0f0f7189bfa4f62917270a05fa9f769c37
Xuefer <xuefer@gmail.com>
2015-07-18 13:13:05 +0800

    audio: include ffmpeg/google audio codec

--- device_pisces.mk
+++ device_pisces.mk
@@ -110,8 +110,11 @@ PRODUCT_COPY_FILES += \
 
 # media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(LOCAL_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/media/enctune.conf:system/etc/enctune.conf \
 
 # Audio
