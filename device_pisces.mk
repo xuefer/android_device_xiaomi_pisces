c557b35275fac6ed2036a8d46ddfe5f2352d05fc
Xuefer <xuefer@gmail.com>
2015-09-11 02:28:45 +0800

    init: dont enable mtp by default, fix adb for charger

--- device_pisces.mk
+++ device_pisces.mk
@@ -18,7 +18,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_CHARACTERISTICS := nosdcard
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mtp,adb \
+    persist.sys.usb.config=adb \
     persist.sys.isUsbOtgEnabled=1 \
 
 PRODUCT_PROPERTY_OVERRIDES += \
